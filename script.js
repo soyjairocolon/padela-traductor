db02879226f3006aaae7fc7f07d38fdacd7cde95
Jairo Colón <83477127+jairocolon@users.noreply.github.com>
2022-08-29 23:32:40 -0500

    Starting consume API

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+//Get the list of languages from the server
+const GET_URL = 'https://text-translator2.p.rapidapi.com/getLanguages';
+
 const OPTIONS = {
-	method: 'get',
+	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '3e40edd2b8msh1d118c5aa78d0bfp15df7ejsnb38062fc4253',
 		'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com',
 	},
 };
 
-fetch('https://text-translator2.p.rapidapi.com/getLanguages', OPTIONS)
-	.then((response) => response.json())
-	.then((response) => console.log(response))
-	.catch((err) => console.error(err));
+fetch(GET_URL, OPTIONS)
+	.then((res) => res.json())
+	.then(
+		(object) => console.log(object)
+		//Code needed to load the Select
+	)
+	.catch((err) => console.log(err));
